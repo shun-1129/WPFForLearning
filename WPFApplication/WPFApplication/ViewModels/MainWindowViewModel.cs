c7378c7185abf3009a933e63e41b6cdc254808b2
shun-1129 <shun.1129.y@gmail.com>
2023-12-16 20:13:53 +0900

    first commit

---
+++ WPFApplication/WPFApplication/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,37 @@
+﻿using WPFSelfLibrary.ViewModelBase;
+using WPFSelfLibrary.Command;
+
+namespace WPFApplication.ViewModels
+{
+    #region Class MainWindowViewModel HeaderComment
+    /// <summary>
+    /// 
+    /// </summary>
+    #endregion
+    public class MainWindowViewModel : ViewModelBase
+    {
+        #region メンバ変数
+        #endregion
+
+        #region プロパティ
+        #endregion
+
+        #region 定数
+        #endregion
+
+        #region コンストラクタ
+        #region MainWindowViewModel HeaderComment
+        /// <summary>
+        /// デフォルトコンストラクタ
+        /// </summary>
+        #endregion
+        public MainWindowViewModel() { }
+        #endregion
+
+        #region 公開メソッド
+        #endregion
+
+        #region 内部メソッド
+        #endregion
+    }
+}
---
+++ WPFApplication/WPFApplication/Views/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+
+namespace WPFApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region メンバ変数
+        #endregion
+
+        #region プロパティ
+        #endregion
+
+        #region 定数
+        #endregion
+
+        #region コンストラクタ
+        #region MainWindow HeaderComment
+        /// <summary>
+        /// デフォルトコンストラクタ
+        /// </summary>
+        #endregion
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region 公開メソッド
+        #endregion
+
+        #region 内部メソッド
+        #endregion
+    }
+}
